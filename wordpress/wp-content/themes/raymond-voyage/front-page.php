ff9d85ec4e2131f62cb7ef6b400a46a754acb00f
Maxime Gendron <maxime.gendron07@gmail.com>
2019-03-17 19:06:37 -0400

    front-page.php

---
+++ wordpress/wp-content/themes/raymond-voyage/front-page.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package raymond-voyage
+ */
+
+get_header();
+?>
+	<div class="container">
+		<div id="primary" class="content-area">
+				<main id="main" class="site-main">
+
+				<?php
+				while ( have_posts() ) :
+					the_post();
+
+					get_template_part( 'template-parts/content', 'page' );
+
+					// If comments are open or we have at least one comment, load up the comment template.
+					// if ( comments_open() || get_comments_number() ) :
+					// 	comments_template();
+					// endif;
+
+                endwhile; // End of the loop.
+                
+                $args = array(
+                    'post_type' => 'post',
+                    'posts_per_page' => 3
+                );
+                    
+                $derniersArticles = new WP_Query($args);
+
+                $argsForfait = array(
+                    'post_type' => 'forfaits',
+                    'posts_per_page' => 2
+                );
+                    
+                $derniersForfaits = new WP_Query($argsForfait);
+
+                if ( $derniersForfaits->have_posts() ) :
+                    ?>
+                    <div class="row">
+                        <?php
+                        while ( $derniersForfaits->have_posts() ) {
+                        $derniersForfaits->the_post();
+                        get_template_part( 'template-parts/excerpt', get_post_type() );
+                        }?>
+                    </div>
+                <?php endif;
+                
+                if ( $derniersArticles->have_posts() ) :
+                    ?>
+                    <div class="row">
+                        <?php
+                        while ( $derniersArticles->have_posts() ) {
+                        $derniersArticles->the_post();
+                        get_template_part( 'template-parts/excerpt', get_post_type() );
+                        }?>
+                    </div>
+                <?php endif;
+                    
+				?>
+
+				</main><!-- #main -->
+		</div><!-- #primary -->
+	</div>
+
+<?php
+// get_sidebar();
+get_footer();
